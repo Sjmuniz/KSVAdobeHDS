faedf197436c96ed8d609484d84be550bf33fe32
Esteban Torre <esteban.torre@gmail.com>
2015-07-24 14:41:00 +0200

    Increased fixwindow seems to smooth things :)

--- vtx.sh
+++ vtx.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-php adobeHDS.php --manifest http://ffahds-lh.akamaihd.net/z/vorvideo01hds_1@305259/manifest.f4m?hdcore=3.7.0 --quality medium --referrer "http://vmf.edge-apps.net/embed/universal?asset=vorterix_video1-20057&type=live&autoplay=true" --useragent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36" 
+php adobeHDS.php --manifest http://ffahds-lh.akamaihd.net/z/vorvideo01hds_1@305259/manifest.f4m?hdcore=3.7.0 --quality medium --fixwindow 2000 --referrer "http://vmf.edge-apps.net/embed/universal?asset=vorterix_video1-20057&type=live&autoplay=true" --useragent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36" 
